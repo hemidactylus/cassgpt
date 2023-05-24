39ab8a25622a3e62b55281c86bd43c553657b24a
Stefano Lottini <stefano.lottini@datastax.com>
2023-05-25 01:32:49 +0200

    langchain+cassIO version for demonstration purposes

--- db.py
+++ db.py
@@ -1,82 +1,14 @@
-from typing import Any, Dict, List
 from cassandra.cluster import Cluster
-from cassandra.query import SimpleStatement
 
+def getSession(**kwargs):
+    cluster = Cluster(**kwargs)
+    session = cluster.connect()
+    return session
 
-class DB:
-    def __init__(self, keyspace: str, table: str, **kwargs):
-        self.keyspace = keyspace
-        self.table = table
-        self.cluster = Cluster(**kwargs)
-        self.session = self.cluster.connect()
-
-        # Create keyspace if not exists
-        self.session.execute(
-            f"""
-            CREATE KEYSPACE IF NOT EXISTS {keyspace}
-            WITH REPLICATION = {{ 'class': 'SimpleStrategy', 'replication_factor': 1 }}
-            """
-        )
-
-        # Create table if not exists
-        self.session.execute(
-            f"""
-            CREATE TABLE IF NOT EXISTS {keyspace}.{table} (
-            id text PRIMARY KEY,
-            start float,
-            end float,
-            title text,
-            text text,
-            url text,
-            published text,
-            channel_id text,
-            embedding FLOAT VECTOR[1536]);
-            """
-        )
-
-        # Create SAI index if not exists
-        sai_index_name = f"{table}_embedding_idx"
-        self.session.execute(
-            f"""
-            CREATE CUSTOM INDEX IF NOT EXISTS {sai_index_name} ON {keyspace}.{table} (embedding)
-            USING 'org.apache.cassandra.index.sai.StorageAttachedIndex'
-            """
-        )
-
-    def upsert_one(self, data):
-            query = SimpleStatement(
-                f"""
-                INSERT INTO {self.keyspace}.{self.table}
-                (id, start, end, title, text, url, published, channel_id, embedding)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """
-            )
-            self.session.execute(
-                query, (
-                    data['id'],
-                    data['start'],
-                    data['end'],
-                    data['title'],
-                    data['text'],
-                    data['url'],
-                    data['published'],
-                    data['channel_id'],
-                    data['embedding']
-                )
-            )
-
-
-    def upsert_batch(self, meta_batch: List[Dict[str, Any]], embeds: List[List[float]]):
-        for meta, vector in zip(meta_batch, embeds):
-            d = meta.copy()
-            d['embedding'] = vector
-            self.upsert_one(d)
-
-    def query(self, vector: List[float], top_k: int) -> List[str]:
-        query = SimpleStatement(
-            f"SELECT id, start, end, text FROM {self.keyspace}.{self.table} WHERE embedding ANN OF %s LIMIT %s"
-        )
-        res = self.session.execute(query, (vector, top_k))
-        rows = [row for row in res]
-        # print('\n'.join(repr(row) for row in rows))
-        return [row.text for row in rows]
+def createKeyspace(session, keyspace):
+    session.execute(
+        f"""
+        CREATE KEYSPACE IF NOT EXISTS {keyspace}
+        WITH REPLICATION = {{ 'class': 'SimpleStrategy', 'replication_factor': 1 }}
+        """
+    )
--- gen-qa-openai.py
+++ gen-qa-openai.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 import argparse
 import os
 import sys
@@ -7,56 +8,69 @@ import openai
 from tqdm.auto import tqdm
 from datasets import load_dataset
 
-from db import DB
-from ai import embedding_of_many, embedding_of, complete
+from langchain.schema import Document
+from langchain.vectorstores.cassandra import Cassandra
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
 
+import cassio
+cassio.globals.enableExperimentalVectorSearch()
 
-openai.api_key = open('openai.key', 'r').read().splitlines()[0]
+from db import getSession, createKeyspace
+
+os.environ['OPENAI_API_KEY'] = open('openai.key', 'r').read().splitlines()[0]
 embed_model = "text-embedding-ada-002"
-db = DB("demo", "youtube_transcriptions")
 
 parser = argparse.ArgumentParser(description="Retrieve data and generate answers")
 parser.add_argument('--load_data', action='store_true', help="Load data if required")
 args = parser.parse_args()
 
+#
+session = getSession()
+createKeyspace(session, 'demo_lc')
+
+llm = OpenAI(temperature=0)
+myEmbedding = OpenAIEmbeddings(model=embed_model)
+
+myCassandraVStore = Cassandra(
+    embedding=myEmbedding,
+    session=session,
+    keyspace='demo_lc',
+    table_name='youtube_transcriptions',
+)
+
+index = VectorStoreIndexWrapper(vectorstore=myCassandraVStore)
+
 if args.load_data:
     data = load_dataset('jamescalam/youtube-transcriptions', split='train')
+    # collate texts into documents by video_id, one doc per video
+    groups = groupby(data, key=lambda entry: entry['video_id'])
+    lcDocuments = []
+    for _, grouper in groups:
+        chunks = list(grouper)
+        fulltext = ' '.join(
+            chk['text']
+            for chk in chunks
+        )
+        metadata = {
+            k: v
+            for k, v in chunks[0].items()
+            if k != 'text'
+        }
+        lcDocuments.append(Document(
+            page_content=fulltext,
+            metadata=metadata,
+        ))
 
-    new_data = []
-    window = 20  # number of sentences to combine
-    stride = 4  # number of sentences to 'stride' over, used to create overlap
+    # # DEMO MODE, we keep 3 videos only
+    # lcDocuments = lcDocuments[:3]
 
-    print('loading data')
-    for i in tqdm(range(0, len(data), stride)):
-        i_end = min(len(data)-1, i+window)
-        if data[i]['title'] != data[i_end]['title']:
-            # in this case we skip this entry as we have start/end of two videos
-            continue
-        text = ' '.join(data[i:i_end]['text'])
-        # create the new merged dataset
-        new_data.append({
-            'start': data[i]['start'],
-            'end': data[i_end]['end'],
-            'title': data[i]['title'],
-            'text': text,
-            'id': data[i]['id'],
-            'url': data[i]['url'],
-            'published': data[i]['published'],
-            'channel_id': data[i]['channel_id']
-        })
-    print(f"Created {len(new_data)} new entries from {len(data)} original entries")
+    mySplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=120)
+    splitDocs = mySplitter.transform_documents(lcDocuments)
 
-    batch_size = 100  # how many embeddings we create and insert at once
-
-    print('generating embeddings')
-    for i in tqdm(range(0, len(new_data), batch_size)):
-        # find end of batch
-        i_end = min(len(new_data), i+batch_size)
-        meta_batch = new_data[i:i_end]
-        # get texts to encode
-        texts = [x['text'] for x in meta_batch]
-        embeds = embedding_of_many(texts, embed_model)
-        db.upsert_batch(meta_batch, embeds)
+    myCassandraVStore.add_documents(splitDocs)
 
 # Now we search
 query = f"Which training method should I use for sentence transformers when " + \
@@ -65,34 +79,6 @@ query = f"Which training method should I use for sentence transformers when " + 
 print(f'Query? (enter to use default)\n\nDefault:\n{query}\n\n> ', end='')
 query = sys.stdin.readline().strip() or query
 
-def enrich(query):
-    # get relevant contexts (including the questions) and add them to the openai prompt
-    limit = 3750
-    xq = embedding_of(query, embed_model)
-    contexts = db.query(xq, top_k=3)
-    print(f"Retrieved {len(contexts)} contexts after asking for 3")
+response = index.query(query)
 
-    # build our prompt with the retrieved contexts included
-    prompt_start = (
-        "Answer the question based on the context below.\n\n"+
-        "Context:\n"
-    )
-    prompt_end = (
-        f"\n\nQuestion: {query}\nAnswer:"
-    )
-    # append contexts until hitting limit
-    for i in range(1, len(contexts) + 1):
-        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
-            prompt_middle = "\n\n---\n\n".join(contexts[:i-1])
-            break
-    else:
-        prompt_middle = "\n\n---\n\n".join(contexts)
-
-    return prompt_start + prompt_middle + prompt_end
-
-# first we retrieve relevant items from the database
-query_with_contexts = enrich(query)
-# then we complete the context-infused query
-print(query_with_contexts)
-response = complete(query_with_contexts)
 print(f'Answer: {response}')
